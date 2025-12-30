b083135b1663dafe3d263e96316454276a1ed691
ginoximain-sketch <ginoximain@gmail.com>
2025-12-31 00:18:22 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://*.supabase.co;"
+          }
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = nextConfig
